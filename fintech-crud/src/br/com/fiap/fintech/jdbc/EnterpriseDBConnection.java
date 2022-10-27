2ac3191e83b8282beafdeb6b5f20d106f9e0f35a
cgodevs <kharol.go@gmail.com>
2022-10-27 19:49:40 -0300

    Remove credentials

--- fintech-crud/src/br/com/fiap/fintech/jdbc/EnterpriseDBConnection.java
+++ fintech-crud/src/br/com/fiap/fintech/jdbc/EnterpriseDBConnection.java
@@ -23,8 +23,8 @@ public class EnterpriseDBConnection {
 
 			connection = DriverManager.getConnection(
 					"jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL",
-					"RM95511", 
-					"210696"
+					"user", 
+					"pw"
 			);
 			
 			return connection;
